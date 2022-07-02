14be6629ebc8678ad79e97819eab3688c5e84e02
anthonylst6 <anthony.liu@tutamail.com>
2022-07-02 22:42:45 +1000

    standardised file back to example

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -6,28 +6,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cmocean
 
+
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
-   
+    
     Args:
       darray (xarray.DataArray): Precipitation data
-    
+   
     """
-    
+   
     darray.data = darray.data * 86400
     darray.attrs['units'] = 'mm/day'
    
     return darray
 
 
+def apply_mask(darray, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+   
+    Args:
+      darray (xarray.DataArray): Data to mask
+      sftlf_file (str): Land surface fraction file
+      realm (str): Realm to mask
+   
+    """
+  
+    dset = xr.open_dataset(sftlf_file)
+  
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data > 50)   
+   
+    return masked_darray
+
+
 def create_plot(clim, model, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
-   
+    
     Args:
       clim (xarray.DataArray): Precipitation climatology data
       model (str): Name of the climate model
       season (str): Season
-      
+     
     Kwargs:
       gridlines (bool): Select whether to plot gridlines
       levels (list): Tick marks on the colorbar    
@@ -36,7 +57,7 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
 
     if not levels:
         levels = np.arange(0, 13.5, 1.5)
-        
+       
     fig = plt.figure(figsize=[12,5])
     ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
     clim.sel(season=season).plot.contourf(ax=ax,
@@ -53,38 +74,6 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
     plt.title(title)
 
 
-# def apply_mask(clim, SFTLF_FILE, REALM):
-#     dset = xr.open_dataset(SFTLF_FILE)
-#     sftlf = dset['sftlf']
-#     if REALM == "ocean":
-#         return clim.where(sftlf.data > 50)
-#     elif REALM == "land":
-#         return clim.where(sftlf.data < 50)
-#     else:
-#         sys.exit("REALM must be either 'land' or 'ocean'")
-
-def apply_mask(darray, sftlf_file, realm):
-    """Mask ocean or land using a sftlf (land surface fraction) file.
-    
-    Args:
-        darray (xarray.DataArray): Data to mask
-        sftlf_file (str): Land surface fraction file
-        realm (str): Realm to mask
-    
-    """
-    
-    dset = xr.open_dataset(sftlf_file)
-    
-    if realm == 'land':
-        masked_darray = darray.where(dset['sftlf'].data < 50)
-    elif realm == "ocean":
-        masked_darray = darray.where(dset['sftlf'].data > 50)
-    else:
-        sys.exit("REALM must be either 'land' or 'ocean'")
-    
-    return masked_darray
-    
-    
 def main(inargs):
     """Run the program."""
 
@@ -93,8 +82,6 @@ def main(inargs):
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
 
-#   clim = apply_mask(clim, inargs.mask[0], inargs.mask[1])
-    
     if inargs.mask:
         sftlf_file, realm = inargs.mask
         clim = apply_mask(clim, sftlf_file, realm)
@@ -109,15 +96,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description)
    
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
-    parser.add_argument("season", type=str, choices=['DJF', 'MAM', 'JJA', 'SON'], help="Season to plot")
+    parser.add_argument("season", type=str, help="Season to plot")
     parser.add_argument("output_file", type=str, help="Output file name")
 
     parser.add_argument("--gridlines", action="store_true", default=False,
                         help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
                         help='list of levels / tick marks to appear on the colorbar')
-    parser.add_argument("--mask", type=str, nargs=2, metavar=('SFTLF_FILE', 'REALM'), default=None, help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
+    parser.add_argument("--mask", type=str, nargs=2,
+                        metavar=('SFTLF_FILE', 'REALM'), default=None,
+                        help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
 
     args = parser.parse_args()
-   
+  
     main(args)
+
